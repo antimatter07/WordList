2b6112f44deefd70ae7a0b6b976dd933d3410511
Matthew Villarica <matthewvillarica@gmail.com>
2021-09-04 18:49:07 +0800

    misc

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -140,15 +140,5 @@ public class BinarySearchTree {
         root = null;
     }
 
-    public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();
-
-        bst.insert("cat");
-        bst.insert("dog");
-        bst.insert("apple");
-        bst.insert("hi");
-       
-        bst.inOrderWalkStart();
-    }
 
 }
